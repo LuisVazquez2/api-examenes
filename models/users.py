b7be2502f876c5c10ee3d35d5683a6f015528a6f
Luis Vazquez <luisito986.22@gmail.com>
2023-04-28 01:44:14 -0600

    update model and migrations

--- models/users.py
+++ models/users.py
@@ -3,31 +3,35 @@ from database.db import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
-    lastName = db.Column(db.String(100), nullable=True)
+    lastName = db.Column(db.String(100), nullable=False)
     secondLastName = db.Column(db.String(100), nullable=True)
-    email = db.Column(db.String(100), nullable=True)
-    password = db.Column(db.String(100), nullable=True)
-    # role = db.Column(db.String(100), nullable=False)
-    # status = db.Column(db.String(100), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    # user-student, admin-teacher, super-admin
+    role = db.Column(db.String(100), nullable=False, default='student')
+    active = db.Column(db.Boolean, nullable=False, default=True)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, nullable=True,
                            onupdate=db.func.current_timestamp())
-    # deleted_at = db.Column(db.DateTime, nullable=False)
 
-    # def init method with properties optional
-    def __init__(self, name, lastName, secondLastName, email, password):
+    def __init__(self, name, lastName, secondLastName, email, password, role='user', active=True):
         self.name = name
         self.lastName = lastName
         self.secondLastName = secondLastName
         self.email = email
         self.password = password
-        # self.role = role
-        # self.status = status
-        self.created_at = db.func.current_timestamp()
-        # self.updated_at = updated_at
-        # self.deleted_at = deleted_at
+        self.role = role
+
+    def __init__(self, name, lastName, password, secondLastName=None, email=None, role='user', active=True):
+        self.name = name
+        self.lastName = lastName
+        self.secondLastName = secondLastName
+        self.email = email
+        self.password = password
+        self.role = role
 
     def __repr__(self):
         return '<User %r>' % self.name
@@ -40,17 +44,5 @@ class User(db.Model):
             'secondLastName': self.secondLastName,
             'email': self.email,
             'password': self.password,
-            'created_at': self.created_at,
-        }
-
-    def serialize_with_token(self, token):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'lastName': self.lastName,
-            'secondLastName': self.secondLastName,
-            'email': self.email,
-            'password': self.password,
-            'created_at': self.created_at,
-            'token': token
+            'role': self.role,
         }
--- routes/users.py
+++ routes/users.py
@@ -1,15 +1,33 @@
-from flask import Blueprint, jsonify , request
+from flask import Blueprint, jsonify, request
 from database.db import db
 from models.users import User
 
 users = Blueprint('users', __name__)
 
-@users.route('/', methods=['GET', 'POST'])
+
+@users.route('/', methods=['POST'])
 def index():
     data = request.get_json()
-    if request.method == 'POST':
-        user = User(data['name'], data['lastName'], data['secondLastName'], data['email'], data['password'])
-        db.session.add(user)
-        db.session.commit()
-        #return a message and the user
-        return jsonify({'message': 'User created successfully!', 'user': user.serialize()}), 201
+
+    # validate the duplicated email
+    user = User.query.filter_by(email=data['email']).first()
+    if user:
+        return jsonify({'message': 'The email already exists'}), 409
+
+    # create the user
+    user = User(
+        name=data['name'] if 'name' in data else None,
+        lastName=data['lastName'] if 'lastName' in data else None,
+        secondLastName=data['secondLastName'] if 'secondLastName' in data else None,
+        email=data['email'] if 'email' in data else None,
+        password=data['password'] if 'password' in data else None,
+        role=data['role'] if 'role' in data else None,
+    )
+    db.session.add(user)
+    db.session.commit()
+    # return a message and the user
+    return jsonify({'message': 'User created successfully!',
+                    'data': {
+                        'id': user.id,
+                    }
+                    }), 201
